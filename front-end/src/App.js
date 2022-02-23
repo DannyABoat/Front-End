4472e4b5f54bc619c30a1f0f52d217a32c1040e1
DannyABoat <dannysum07@yahoo.com>
2022-02-23 15:52:20 +0000

    Front end with Sign Up functionality

---
+++ front-end/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {useState} from "react";
+import { fetchRequest } from './utils';
+
+const App = () => {
+    const [user, setUser] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const submitHandler = async (e) => {
+      e.preventDefault();
+      fetchRequest(setUser, username, email, password);
+    }
+
+  return (
+    <div>
+      <h1>{user ? `Welcome ${user}` : "Please Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <input onChange={(e) => setUsername(e.target.value)} placeholder="Username"/>
+        <input onChange={(e => setEmail(e.target.value))} placeholder="Email"/>
+        <input onChange={(e => setPassword(e.target.value))} placeholder="Password"/>
+      <button type="submit"> Sign Up </button>
+      </form>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/utils/index.js
@@ -0,0 +1,17 @@
+export const fetchRequest = async (setUser, username, email, password) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+            method:  "POST",
+            headers: { "Content-Type" : "application/json"},
+            body: JSON.stringify({
+                username: username,
+                email: email, 
+                password: password
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
